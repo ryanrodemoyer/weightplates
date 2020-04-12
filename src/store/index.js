dc6aaea65090b0330545c2a9254a8fc7764da9c2
Ryan Rodemoyer <ryanrodemoyer@Ryans-MBP.T-mobile.com>
2020-04-12 10:42:21 -0600

    defaultOptions.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import defaultOptions from "./defaultOptions.js";
+
 Vue.use(Vuex);
 
 const calculate = (inventory, barWeight, targetWeight) => {
@@ -55,50 +57,7 @@ for (let i = BAR + 5; i <= 405; i += 5) {
 }
 
 const localState = {
-  options: [
-    {
-      id: 1,
-      weight: 45,
-      quantity: 1,
-      display: "45 lb"
-    },
-    {
-      id: 2,
-      weight: 35,
-      quantity: 1,
-      display: "35 lb"
-    },
-    {
-      id: 3,
-      weight: 25,
-      quantity: 1,
-      display: "25 lb"
-    },
-    {
-      id: 4,
-      weight: 15,
-      quantity: 1,
-      display: "15 lb"
-    },
-    {
-      id: 5,
-      weight: 10,
-      quantity: 1,
-      display: "10 lb"
-    },
-    {
-      id: 6,
-      weight: 5,
-      quantity: 1,
-      display: "5 lb"
-    },
-    {
-      id: 7,
-      weight: 2.5,
-      quantity: 1,
-      display: "2.5 lb"
-    }
-  ]
+  options: defaultOptions
 };
 
 const localMutations = {
