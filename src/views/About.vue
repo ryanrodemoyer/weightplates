f9f6cc8243f761416bdd42d69c916c70f4533854
Ryan <ryan.rodemoyer@icloud.com>
2020-04-22 20:29:02 -0600

    fix the messages on About

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,23 +19,20 @@
       <h3>Instructions</h3>
       <p>
         1. Use the Available Plates group to provide how many plates are
-        available in your setup. Your changes are stored directly in your
-        browser!
+        available in your setup. Changes are stored directly in your browser for
+        quick access in the future!
       </p>
       <p>
         2. The Results group shows how many pairs of each plate are required to
         meet the target weight. Many combinations are calculated but only one
         option is shown when your device is in portrait mode. The app will show
-        all combinations if you rotate to landscape.
+        all combinations when rotated to landscape.
       </p>
     </div>
   </div>
 </template>
 
 <style type="scss">
-.center {
-  text-align: center;
-}
 h2 {
   font-style: italic;
 }
